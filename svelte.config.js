77c31a2b669a20eee76e5b33a8915456e6bb5e19
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-02-27 13:31:38 +0530

    svelte config changed

--- svelte.config.js
+++ svelte.config.js
@@ -10,15 +10,18 @@ const config = {
     postcss: true,
   }),
 
+  prerender: {
+    enabled: false
+  },
+  ssr: false,
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     adapter: adapter({
       // default options are shown
       pages: 'build',
       assets: 'build',
       fallback: null,
-      precompress: false,
+      precompress: false
     }),
     vite: {
       resolve: {
